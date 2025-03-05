8c1d4226285d7da6cb83bf7b816bc4a50374b164
Nishant Ranjan <nishant.ranjan@roosterinc.com>
2025-03-06 04:26:11 +0530

    Redirect home page to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import ExpenseTracker from "../components/ExpenseTracker";
-import ExpensePieChart from "@/components/ExpensePieChart";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="min-h-screen p-8 pb-20 sm:p-20 relative">
-      <ExpenseTracker />
-      <ExpensePieChart />
-    </div>
-  );
+  redirect("/dashboard");
 }
